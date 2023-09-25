0b330a8492393fd762bf603bc5a031f9ef417fa7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-09-25 17:00:08 +0000

    Updating to latest FlutterFlow output.

--- lib/teste_drop/teste_drop_widget.dart
+++ lib/teste_drop/teste_drop_widget.dart
@@ -115,7 +115,10 @@ class _TesteDropWidgetState extends State<TesteDropWidget> {
               ),
               FutureBuilder<List<EstadosRow>>(
                 future: EstadosTable().queryRows(
-                  queryFn: (q) => q,
+                  queryFn: (q) => q.eq(
+                    'nome_estado',
+                    _model.dropDownValue1,
+                  ),
                 ),
                 builder: (context, snapshot) {
                   // Customize what your widget looks like when it's loading.
@@ -163,6 +166,36 @@ class _TesteDropWidgetState extends State<TesteDropWidget> {
                     isMultiSelect: false,
                   );
                 },
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  FFButtonWidget(
+                    onPressed: () {
+                      print('Button pressed ...');
+                    },
+                    text: 'SALVAR',
+                    options: FFButtonOptions(
+                      height: 40.0,
+                      padding:
+                          EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
+                      iconPadding:
+                          EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                      color: FlutterFlowTheme.of(context).primary,
+                      textStyle:
+                          FlutterFlowTheme.of(context).titleSmall.override(
+                                fontFamily: 'Readex Pro',
+                                color: Colors.white,
+                              ),
+                      elevation: 3.0,
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                        width: 1.0,
+                      ),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
